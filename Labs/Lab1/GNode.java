3526a862d7888509ab7f1dfdea4bdc8217297b3c
Michelle Paradis <mpara102@uottawa.ca>
2020-01-07 20:19:55 -0500

    oops

--- Labs/Lab1/GNode.java
+++ Labs/Lab1/GNode.java
@@ -9,31 +9,32 @@
  */
 //begin#fragment Node
 public class GNode<E> {
-  // Instance variables:
-  private E element;
-  private GNode<E> next;
-  /** Creates a node with null references to its element and next node. */
-  public GNode() {
-    this(null, null);
-  }
-  /** Creates a node with the given element and next node. */
-  public GNode(E e, GNode<E> n) {
-    element = e;
-    next = n;
-  }
-  // Accessor methods:
-  public E getElement() {
-    return element; 
-  }
-  public GNode<E> getNext() { 
-    return next;
-  }
-  // Modifier methods:
-  public void setElement(E newElem) { 
-    element = newElem; 
-  }
-  public void setNext(GNode<E> newNext) {
-    next = newNext; 
-  }
+	// Instance variables:
+	private E element;
+	private GNode<E> next;
+	/** Creates a node with null references to its element and next node. */
+	public GNode() {
+		this(null, null);
+	}
+	/** Creates a node with the given element and next node. */
+	public GNode(E e, GNode<E> n) {
+		element = e;
+		next = n;
+	}
+	// Accessor methods:
+	public E getElement() {
+		return element; 
+	}
+	
+	public GNode<E> getNext() { 
+		return next;
+	}
+	// Modifier methods:
+	public void setElement(E newElem) { 
+		element = newElem; 
+	}
+	public void setNext(GNode<E> newNext) {
+		next = newNext; 
+	}
 }
 //end#fragment Node
