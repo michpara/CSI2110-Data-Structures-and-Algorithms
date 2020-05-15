d6a8a2dd03b0046275ef2f8a19aaa080bd73589d
Michelle Paradis <mpara102@uottawa.ca>
2020-05-15 18:00:54 -0400

    start of lab4

---
+++ Labs/Lab4/HeapPriorityQueue.java
@@ -0,0 +1,141 @@
+/**
+ * Array Heap implementation of a priority queue
+ * @author Lachlan Plant
+ */
+public class HeapPriorityQueue<K extends Comparable,V> implements PriorityQueue<K ,V> {
+    
+    private Entry[] storage; //The Heap itself in array form
+    private int tail;        //Index of last element in the heap
+    
+    /**
+    * Default constructor
+    */
+    public HeapPriorityQueue(){
+    	this(100);
+    }
+    
+    /**
+    * HeapPriorityQueue constructor with max storage of size elements
+    */
+    public HeapPriorityQueue(int size){
+    	storage = new Entry[size];
+    	tail = -1;
+    }
+    
+    /****************************************************
+     * 
+     *             Priority Queue Methods
+     * 
+     ****************************************************/
+    
+    /**
+    * Returns the number of items in the priority queue.
+    * O(1)
+    * @return number of items
+    */
+    public int size(){
+        return tail + 1;
+    }
+
+    /**
+    * Tests whether the priority queue is empty.
+    * O(1)
+    * @return true if the priority queue is empty, false otherwise
+    */
+    public boolean isEmpty(){
+        return tail < 0;
+    }
+    
+    /**
+    * Inserts a key-value pair and returns the entry created.
+    * O(log(n))
+    * @param key     the key of the new entry
+    * @param value   the associated value of the new entry
+    * @return the entry storing the new key-value pair
+    * @throws IllegalArgumentException if the heap is full
+    */
+    public Entry<K,V> insert(K key, V value) throws IllegalArgumentException{
+    	if(tail+1 == storage.length) {
+    		throw new IllegalArgumentException();
+    	}
+    	Entry entry = new Entry(key, value);
+    	storage[tail+1] = entry;
+    	tail++;
+    	return entry;
+    }
+    
+    /**
+    * Returns (but does not remove) an entry with minimal key.
+    * O(1)
+    * @return entry having a minimal key (or null if empty)
+    */
+    public Entry<K,V> min(){
+    	if(isEmpty()) {
+    		return null;
+    	}
+        return storage[0];
+    } 
+    
+    /**
+    * Removes and returns an entry with minimal key.
+    * O(log(n))
+    * @return the removed entry (or null if empty)
+    */ 
+    public Entry<K,V> removeMin(){
+    	if(isEmpty()) {
+    		return null;
+    	}
+        Entry temp = storage[0];
+        storage[0] = null;
+        return temp;
+    }  
+    
+    
+    /****************************************************
+     * 
+     *           Methods for Heap Operations
+     * 
+     ****************************************************/
+    
+    /**
+    * Algorithm to place element after insertion at the tail.
+    * O(log(n))
+    */
+    private void upHeap(int location){
+         return;          
+    }
+    
+    /**
+    * Algorithm to place element after removal of root and tail element placed at root.
+    * O(log(n))
+    */
+    private void downHeap(int location){
+    	int root = 0;
+    	s
+    }
+    
+    /**
+    * Find parent of a given location,
+    * Parent of the root is the root
+    * O(1)
+    */
+    private int parent(int location){
+    	int parent;
+        if(location%2==0) {
+        	parent = location-2/2;
+        }else {
+        	parent = location-1/2;
+        }
+        return parent;
+    }
+    
+   
+    /**
+    * Inplace swap of 2 elements, assumes locations are in array
+    * O(1)
+    */
+    private void swap(int location1, int location2){
+        return;  
+    }
+    
+}
